4e0da9304ef4947526ad3dc5253f3cde37582008
Timoteo Rafael Rodrigues <timoteorafael@outlook.com>
2017-08-22 20:49:58 -0300

    Entity

---
+++ KTech.VirtualStore.Domain/Repository/EfDbContext.cs
@@ -0,0 +1,23 @@
+﻿using KTech.VirtualStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace KTech.VirtualStore.Domain.Repository
+{
+    public class EfDbContext: DbContext
+    {
+        public DbSet<Produto> Produtos { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Entity<Produto>().ToTable("Produtos");
+        }
+    }
+}
---
+++ KTech.VirtualStore.Domain/Repository/ProdutosRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KTech.VirtualStore.Domain.Entities;
+namespace KTech.VirtualStore.Domain.Repository
+{
+    public class ProdutosRepository
+    {
+        private readonly EfDbContext _context = new EfDbContext();
+
+        public IEnumerable<Produto> Produtos
+        {
+            get { return _context.Produtos; }
+        }
+    }
+}
