971d6f3b0ab66454199ad7547a02ffdabaad645a
Timoteo Rafael Rodrigues <timoteorafael@outlook.com>
2017-08-22 11:52:42 -0300

    Add project files.

---
+++ KTech.VirtualStore.UnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KTech.VirtualStore.UnitTest
+{
+    [TestClass]
+    public class UnitTestKTech
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ KTech.VirtualStore.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KTech.VirtualStore.Web.MvcApplication" Language="C#" %>
